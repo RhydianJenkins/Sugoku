2b5c5bfe6892c6d14c22dded69563d8a8149b6cf
Rhydian Jenkins <rhydian.jenkins@basekit.com>
2024-02-15 17:44:18 +0000

    Allow port to be passed in via cli

--- main.go
+++ main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 
 	"github.com/rhydianjenkins/sugoku/pkg/handlers"
 )
 
+func getPort() *string {
+	port := flag.String("port", "8080", "Port to run the server on")
+	flag.Parse()
+
+	return port
+}
+
 func main() {
+	port := getPort()
+
 	fs := http.FileServer(http.Dir("pkg/public/styles"))
 	http.Handle("/public/styles/", http.StripPrefix("/public/styles/", fs))
 
 	http.HandleFunc("/api/test", handlers.HelloWorldHandler)
 	http.HandleFunc("/", handlers.WebPageHandler)
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":"+*port, nil)
 	if err != nil {
 		panic(err)
 	}
 
-	println("Server listening on port 8080...")
+	fmt.Println("Server listening on port " + *port)
 }
